7c1b8121acfa6e498c2c8f27d3719479869d54d5
dfislamov <dfislamov@rt-dc.ru>
2025-02-03 18:32:45 +0500

    fix set role (from name)

--- config/config.py
+++ config/config.py
@@ -8,9 +8,10 @@ load_dotenv(dotenv_path=dotenv_path, override=True)
 
 
 class SUOP:
-    BASE_URL = os.getenv("BASE_URL").split('//')
-    BASIC_AUTH_LOGIN = os.getenv("BASIC_AUTH_LOGIN")
-    BASIC_AUTH_PASSWORD = os.getenv("BASIC_AUTH_PASSWORD")
-    MAIN_URL = f"{BASE_URL[0]}//{BASIC_AUTH_LOGIN}:{BASIC_AUTH_PASSWORD}@{BASE_URL[1]}"
-    CLIENT_LOGIN = os.getenv("CLIENT_LOGIN")
-    CLIENT_PASSWORD = os.getenv("CLIENT_PASSWORD")
+    BASE_URL = os.getenv('BASE_URL').split('//')
+    BASIC_AUTH_LOGIN = os.getenv('BASIC_AUTH_LOGIN')
+    BASIC_AUTH_PASSWORD = os.getenv('BASIC_AUTH_PASSWORD')
+    MAIN_URL = f'{BASE_URL[0]}//{BASIC_AUTH_LOGIN}:{BASIC_AUTH_PASSWORD}@{BASE_URL[1]}'
+    CLIENT_LOGIN = os.getenv('CLIENT_LOGIN')
+    CLIENT_PASSWORD = os.getenv('CLIENT_PASSWORD')
+    ORGANIZATION_CLIENT = os.getenv('ORGANIZATION_CLIENT')
