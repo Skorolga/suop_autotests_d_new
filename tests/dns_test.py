a17c0606f1e459cfa325214c3e2b079874431949
dfislamov <dfislamov@rt-dc.ru>
2025-02-04 15:52:39 +0500

    add pre and post condition

--- tests/dns_test.py
+++ tests/dns_test.py
@@ -1,11 +1,21 @@
 import time
 import allure
+import pytest
 from src.pages.main_page import MainPage
 from src.pages.auth_page import Auth
 from config.config import SUOP
 
+@pytest.fixture
+def pre_post_dns(browser):
+    print('!!!Предусловие' * 10)
+    yield
+    print('###Постусловие' * 10)
+    auth_page = Auth(browser)
+    auth_page.logout()
+    print('Автотест завершен')
+
 @allure.story('Управление DNS. Добавление домена.')
-def test_dns(browser):
+def test_dns(pre_post_dns, browser):
     """Тест DNS СУ ОП"""
     main_page = MainPage(browser, SUOP.MAIN_URL)  # экземпляр главной страницы с url
     auth_page = Auth(browser)
@@ -20,4 +30,3 @@ def test_dns(browser):
     auth_page.login_as_admin_suop()
     auth_page.wait_for_page_loaded(auth_page.profile_name)
     auth_page.save_scr('Личный кабинет Администратор СУОП')
-    auth_page.logout()
