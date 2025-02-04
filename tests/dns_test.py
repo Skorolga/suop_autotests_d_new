ea49154ac3f58c129eeec481374901f9f4d9fdc8
dfislamov <dfislamov@rt-dc.ru>
2025-02-04 19:38:53 +0500

    add allure reports

--- tests/dns_test.py
+++ tests/dns_test.py
@@ -1,5 +1,6 @@
 import time
 import allure
+from allure_commons.types import AttachmentType
 import pytest
 from src.pages.main_page import MainPage
 from src.pages.auth_page import Auth
@@ -7,26 +8,47 @@ from config.config import SUOP
 
 @pytest.fixture
 def pre_post_dns(browser):
-    print('!!!Предусловие' * 10)
     yield
-    print('###Постусловие' * 10)
     auth_page = Auth(browser)
     auth_page.logout()
-    print('Автотест завершен')
 
+@allure.tag('dns')
+@allure.testcase('https://jira.rt-dc.ru/secure/Tests.jspa#/v2/testCases')
 @allure.story('Управление DNS. Добавление домена.')
 def test_dns(pre_post_dns, browser):
     """Тест DNS СУ ОП"""
     main_page = MainPage(browser, SUOP.MAIN_URL)  # экземпляр главной страницы с url
     auth_page = Auth(browser)
 
-    main_page.wait_for_page_loaded(main_page.showcase_card)
-    main_page.save_scr('main_page')
-    main_page.auth_main_page()
-    main_page.wait_for_page_loaded(auth_page.submit_btn)
-    main_page.save_scr('form_page')
-    auth_page.login()
-    auth_page.save_scr('Личный кабинет клиент')
-    auth_page.login_as_admin_suop()
-    auth_page.wait_for_page_loaded(auth_page.profile_name)
-    auth_page.save_scr('Личный кабинет Администратор СУОП')
+    with allure.step('Открываем главную страницу'):
+        main_page.wait_for_page_loaded(main_page.showcase_card)
+        allure.attach(
+            body=main_page.browser.get_screenshot_as_png(),
+            name='Главня_страница',
+            attachment_type=AttachmentType.PNG
+        )
+
+    with allure.step('Личный кабинет клиента'):
+        main_page.auth_main_page()
+        main_page.wait_for_page_loaded(auth_page.submit_btn)
+        allure.attach(
+            body=main_page.browser.get_screenshot_as_png(),
+            name='Форма_авторизации',
+            attachment_type=AttachmentType.PNG
+        )
+        auth_page.login()
+        main_page.wait_for_page_loaded(auth_page.profile_name_client)
+        allure.attach(
+            body=main_page.browser.get_screenshot_as_png(),
+            name='Личный_кабинет_клиента',
+            attachment_type=AttachmentType.PNG
+        )
+
+    with allure.step('Личный кабинет администратора'):
+        auth_page.login_as_admin_suop()
+        auth_page.wait_for_page_loaded(auth_page.profile_name_admin)
+        allure.attach(
+            body=main_page.browser.get_screenshot_as_png(),
+            name='Личный_кабинет_Администратор_СУОП',
+            attachment_type=AttachmentType.PNG
+        )
