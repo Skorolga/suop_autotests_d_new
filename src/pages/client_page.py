7494ac752d41046cf9f53990f8b0714283c51cf6
dfislamov <dfislamov@rt-dc.ru>
2025-02-07 21:58:46 +0500

    check cost in form for make order

--- src/pages/client_page.py
+++ src/pages/client_page.py
@@ -8,15 +8,21 @@ class ClientPage(BasicPage):
 
     MENU_MAKE_ORDER = (By.XPATH, '//span[contains(text(), "Заказать услугу")]')
     BANNER_MAKE_ORDER = (By.XPATH, '//h4[contains(text(), "Виртуальная инфраструктура")]')
-    TABLE_WITH_ORDERS_IN_LK = (By.XPATH, '//div[contains(@class, "items-table__dropdown")]')  # проверка загрузки стр.
+    BUTTON_MAKE_ORDER = (By.XPATH, '//button[contains(text(), "Заказать")]')
+    TABLE_WITH_ORDERS_IN_LK = (By.XPATH, '//div[contains(@class, "items-table__dropdown")]')  # проверка загрузки стр. с заказами
+
+    # локаторы заказа за клиента
+    FORM_TITLE_CONF = (By.XPATH, '//h3[contains(text(), "Конфигурация")]')  # Для проверки загрузки страницы с формой заказа iaas
+    COST_WITHOUT_TAX = (By.XPATH, '//div[@class="costs-value"]')
 
     def __init__(self, browser):
         super().__init__(browser)
 
-    def create_order(self):
+    def make_order(self):
         """Метод создает заказ Публичное облако под уже авторизованным клиентом"""
         logger.info('Создание заказа Публичное облако за клиента')
         self.click_on_element(self.MENU_MAKE_ORDER)
         self.wait_for_page_loaded(self.BANNER_MAKE_ORDER)
         self.click_on_element(self.BANNER_MAKE_ORDER)
+        self.click_on_element(self.BUTTON_MAKE_ORDER)
 
