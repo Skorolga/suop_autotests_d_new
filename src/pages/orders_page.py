8cdd6bca41380470d55918ea86d26c8870c43120
dfislamov <dfislamov@rt-dc.ru>
2025-02-12 23:03:29 +0500

    add new del method

--- src/pages/orders_page.py
+++ src/pages/orders_page.py
@@ -3,6 +3,8 @@ from datetime import datetime
 import allure
 from allure_commons.types import AttachmentType
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from src.pages.basic_page import BasicPage
 from src.pages.client_page import ClientPage
 from src.logger.formatted_logger import logger
@@ -22,6 +24,7 @@ class OrdersPage(BasicPage):
     ORDER_BUTTON_APPROVE_MANAGER_2 = (By.XPATH, '//button[contains(text(), "Перевести в тестовый режим")]')  # Еще раз подтверждаем согласование заказа
 
     # заказ
+    ORDER_STATUS = (By.XPATH, '//div[contains(@class, "statusText")]')
     ORDER_STATUS_CHANGE = (By.XPATH, '//div[contains(text(), "Изменение объема ресурсов")]')
     ORDER_STATUS_READY = (By.XPATH, '//div[contains(text(), "Работает")]')
     ORDER_STATUS_SHUTDOWN = (By.XPATH, '//div[contains(text(), "Выключение")]')
@@ -36,13 +39,20 @@ class OrdersPage(BasicPage):
     def __init__(self, browser):
         super().__init__(browser)
 
-    def find_order(self, num_order):
+    def find_order(self, num_order:str):
+        self.browser.refresh()
         self.click_on_element(self.FILTER_CLEAN_BUTTON)  # Сброс фильтров для поиска заказов
-        self.wait_for_page_loaded(ClientPage.TABLE_WITH_ORDERS_IN_LK)
+        WebDriverWait(self.browser, 30).until(EC.invisibility_of_element(self.FILTER_CLEAN_BUTTON))  # ждем когда элемент исчезнет
+        # self.wait_for_page_loaded(ClientPage.TABLE_WITH_ORDERS_IN_LK, 60)
+        time.sleep(2)
         self.click_on_element(self.FILTER_FOR_FIND_ORDERS)
+        time.sleep(2)
         self.send_text(self.FILTER_INPUT_ID_ORDER, num_order)
+        time.sleep(2)
         self.click_on_element(self.FILTER_FORM_APPLY_BUTTON)
         self.wait_for_page_loaded(ClientPage.TABLE_WITH_ORDERS_IN_LK)
+        composite_locator = (By.XPATH, f'//div[contains(@class, "orderRow")]/div[contains(text(), "{num_order}")]')
+        self.wait_for_page_loaded(composite_locator, 60)
 
     def clean_filter_for_find_orders(self):
         """Очистка параметров фильтра поиска страницы с заказами"""
@@ -77,6 +87,55 @@ class OrdersPage(BasicPage):
         self.wait_for_page_loaded(self.ORDER_STATUS_CHANGE, 540)
         self.wait_for_page_loaded(self.ORDER_STATUS_READY, 540)
 
-    def check_order_status(self, num_order, timeout):
-        pass
+    def text_check(self, locator, text_trigger, timeout):
+        start_time = datetime.now()
+        current_text = self.find_elem(locator).text
+        logger.info(f'Состояние заказа: {current_text}')
+        i = 0
+        while True:
+            # установка таймаута
+            time_difference = datetime.now() - start_time
+            if time_difference.total_seconds() > timeout:
+                logger.error('timeout при ожидании изменения статуса')
+                return False
+
+            i += 1
+            logger.info(f'Итерация №: {i} Прошло: {time_difference} сек')
+            self.browser.refresh()
+            actual_text = self.find_elem(locator).text
+            if actual_text == text_trigger:
+                logger.info(f'Ожидаемое состояние достигнуто: {actual_text}')
+                return True
+            elif current_text != actual_text:
+                logger.info(f'Состояние изменилось: {actual_text}')
+                current_text = actual_text
+            else:  # Если найдено триггерное слово завершаем проверку
+                logger.info(f'Состояние не изменилось')
+            time.sleep(10)
+
+    def del_order_dev(self, num_order):
+        logger.info('Удаление заказа del_order_dev()')
+        self.browser.refresh()  # обновляем страницу, баг с появлением УЗ Клиента
+        logger.info('Обновление страницы')
+        self.find_order(num_order)
+        self.click_on_element(self.ORDER_POWER_OFF)
+        self.click_on_element(self.ORDER_POWER_OFF_MODAL_YES)
+        self.text_check(self.ORDER_STATUS, 'Выключен', 60*10)
+        self.browser.refresh()
+        # self.wait_for_page_loaded(self.ORDER_STATUS_SHUTDOWN, 540)
+        # self.browser.refresh()
+        # self.wait_for_page_loaded(self.ORDER_STATUS_STOPPED, 540)
+        # self.browser.refresh()
+        # self.wait_for_page_loaded(self.ORDER_DELETE, 540)
+        self.click_on_element(self.ORDER_DELETE, 60*3)
+        self.click_on_element(self.ORDER_DELETE_MODAL_YES, 60*3)
+        # self.browser.refresh()
+        # self.wait_for_page_loaded(self.ORDER_STATUS_CHANGE, 540)
+        # self.browser.refresh()
+        # self.wait_for_page_loaded(self.ORDER_STATUS_DELETED, 540)
+        self.text_check(self.ORDER_STATUS, 'Удален', 60 * 10)
+
+
+
+
 
---
+++ tests/del_test.py
@@ -0,0 +1,59 @@
+import time
+from asyncio import timeout
+
+import allure
+from allure_commons.types import AttachmentType
+import pytest
+from src.pages.main_page import MainPage
+from src.pages.auth_page import Auth
+from src.pages.client_page import ClientPage
+from src.pages.orders_page import OrdersPage
+from config.config import SUOP
+from src.logger.formatted_logger import logger
+
+@pytest.fixture
+def pre_post_dns(browser):
+    """TODO вынести в отдельную фикстуру, в conftest.py"""
+    yield
+    browser.get(SUOP.MAIN_URL + '/logout')  # TODO логаут по URL, т.к. тест может остановиться на странице где нет меню для выхода, например модальное окно выбора организации
+
+@allure.testcase('https://jira.rt-dc.ru/secure/Tests.jspa#/v2/testCases')
+@allure.story('Управление DNS. Удаление.')
+def test_del(pre_post_dns, browser):
+    """Тест DNS СУ ОП"""
+
+    main_page = MainPage(browser)  # экземпляр главной страницы с url
+    auth_page = Auth(browser)
+    client_page = ClientPage(browser)
+    orders_page = OrdersPage(browser)
+
+    step_name = 'Открываем главную страницу'
+    with allure.step(step_name):
+        logger.info('Шаг: ' + step_name)
+        main_page.go_to(SUOP.MAIN_URL)
+        main_page.wait_for_page_loaded(main_page.SHOWCASE_CARD)
+
+    step_name = 'Личный кабинет клиента'
+    with allure.step(step_name):
+        logger.info('Шаг: ' + step_name)
+        auth_page.auth_as_client()
+        main_page.wait_for_page_loaded(auth_page.PROFILE_NAME_CLIENT)  # Ожидание появление элемента
+        client_page.wait_for_page_loaded(client_page.TABLE_WITH_ORDERS_IN_LK)
+        allure.attach(
+            body=main_page.browser.get_screenshot_as_png(),
+            name='Личный_кабинет_клиента',
+            attachment_type=AttachmentType.PNG
+        )
+
+    step_name = 'Удаление заказа'
+    with allure.step(step_name):
+        logger.info('Шаг: ' + step_name)
+        auth_page.relogin_as_admin_suop()
+        order_num = '119967'
+        orders_page.del_order_dev(order_num)
+        allure.attach(
+            body=main_page.browser.get_screenshot_as_png(),
+            name='Статус заказа удален',
+            attachment_type=AttachmentType.PNG
+        )
+        time.sleep(30)
