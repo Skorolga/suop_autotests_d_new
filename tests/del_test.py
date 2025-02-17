17bfecad82fd0ca9dce7f2cd537293f8d4aa6022
dfislamov <dfislamov@rt-dc.ru>
2025-02-17 19:11:27 +0500

    add new waiting for approve method

--- tests/del_test.py
+++ tests/del_test.py
@@ -44,12 +44,32 @@ def test_del(pre_post_dns, browser):
             name='Личный_кабинет_клиента',
             attachment_type=AttachmentType.PNG
         )
+    order_num = '120496'
+    step_name = 'Согласование созданного заказа за менеджера'
+    with allure.step(step_name):
+        logger.info('Шаг: ' + step_name)
+        auth_page.relogin_as_manager()
+        # orders_page.wait_for_page_loaded(client_page.TABLE_WITH_ORDERS_IN_LK)
+        # allure.attach(
+        #     body=auth_page.browser.get_screenshot_as_png(),
+        #     name='Список заказов',
+        #     attachment_type=AttachmentType.PNG
+        # )
+        # orders_page.clean_filter_for_find_orders()  # Сброс фильтров для поиска заказов
+        orders_page.wait_for_page_loaded(client_page.TABLE_WITH_ORDERS_IN_LK)
+        orders_page.find_order(order_num)
+        allure.attach(
+            body=auth_page.browser.get_screenshot_as_png(),
+            name='Найденный заказ',
+            attachment_type=AttachmentType.PNG
+        )
+        orders_page.approve_order(order_num)
+        orders_page.wait_ready_for_all_child_orders()
 
     step_name = 'Удаление заказа'
     with allure.step(step_name):
         logger.info('Шаг: ' + step_name)
         auth_page.relogin_as_admin_suop()
-        order_num = '120140'
         orders_page.del_order(order_num)
         allure.attach(
             body=main_page.browser.get_screenshot_as_png(),
