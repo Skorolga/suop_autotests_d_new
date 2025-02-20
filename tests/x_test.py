0c93e487a90cc2ee6597cb6dcebd17a974f90ed2
dfislamov <dfislamov@rt-dc.ru>
2025-02-20 22:48:06 +0500

    add assert for domains count

--- tests/x_test.py
+++ tests/x_test.py
@@ -57,6 +57,9 @@ def test_x(pre_post_dns, browser):
         order_page.wait_for_page_loaded(order_page.DOMAIN_TITLE)
         # logger.info(order_page.get_text(order_page.DOMAIN_TITLE))
         assert 'Домены' in order_page.find_elem(order_page.DOMAIN_TITLE).text, 'Заголовок "Домены" не найден'
+        domains_count = int(''.join([i for i in order_page.find_elem(order_page.DOMAIN_TITLE).text if i.isdigit()]))
+        assert domains_count
+        assert domains_count > 0, 'не указано количество доменов в заголовке'
         time.sleep(3)
         allure.attach(
             body=auth_page.browser.get_screenshot_as_png(),
